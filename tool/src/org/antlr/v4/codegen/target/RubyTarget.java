8df4d21a88f677a6442df5933ce76f0f9456f2f3
Tony Walmsley <tony@aosd.co.uk>
2019-05-25 08:51:49 +0100

    Updated license to match the ANTLR4 copyright prior to pull request.

--- tool/src/org/antlr/v4/codegen/target/RubyTarget.java
+++ tool/src/org/antlr/v4/codegen/target/RubyTarget.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2012-2019 The ANTLR Project. All rights reserved.
+ * Use of this file is governed by the BSD 3-clause license that
+ * can be found in the LICENSE.txt file in the project root.
+ */
 package org.antlr.v4.codegen.target;
 
 import org.antlr.v4.Tool;
